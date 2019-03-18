049b2b2c80f425951ca00bba0ad5ad4481a1b0c2
Mike Schenkel <mikeschenkel@gmail.com>
2019-03-18 15:51:47 +0100

    Move deep-clean script

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -160,6 +160,9 @@ alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 # Clean up LaunchServices to remove duplicates in the “Open With” menu
 alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 
+# Combine multiple cleanup scripts
+alias deepclean='cleanup && lscleanup && emptytrash'
+
 # Get week number
 alias week='date +%V'
 
